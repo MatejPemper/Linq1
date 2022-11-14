2f41badd42d07c332ed0589a899a75a400e3c049
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-11-14 10:50:46 +0100

    Add files via upload

---
+++ LinQTest1/LinQTest1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQTest1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // string collection
+            IList<string> stringList = new List<string>() {
+            "C# Tutorials",
+            "VB.NET Tutorials",
+            "Learn C++",
+            "MVC Tutorials" ,
+            "Java"
+        };
+
+            // LINQ Query Syntax
+            var result = from s in stringList
+                         where s.Contains("Tutorials")
+                         select s;
+
+            foreach (var str in result)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
